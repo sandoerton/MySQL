3a9b2c228b6290f6a0750bb23e8800779ff36103
Sandoerton <sandoerton@gmail.com>
2022-07-23 09:24:35 -0300

    Desafio 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,1 +1,15 @@
-SELECT 40 AS cancoes, 6 AS artistas, 10 AS albuns;
+USE SpotifyClone;
+
+SELECT 
+	  COUNT(DISTINCT musicas.musica) AS cancoes,
+    COUNT(DISTINCT artistas.artista) AS artistas,
+    COUNT(DISTINCT albuns.album) AS albuns
+FROM
+    musicas
+        INNER JOIN
+    albuns
+        INNER JOIN
+    artistas
+WHERE
+    musicas.album_id = albuns.id
+        AND albuns.artista_id = artistas.id;
