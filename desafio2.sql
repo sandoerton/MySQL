c7fe0e2f777eb06b953b639e640d7d537ca839eb
Sandoerton <sandoerton@gmail.com>
2022-07-25 19:42:25 -0300

    fix: review desafio 2 e 3

--- desafio2.sql
+++ desafio2.sql
@@ -1,15 +1,10 @@
-USE SpotifyClone;
-
 SELECT 
-	  COUNT(DISTINCT musicas.musica) AS cancoes,
+    COUNT(DISTINCT musicas.musica) AS cancoes,
     COUNT(DISTINCT artistas.artista) AS artistas,
     COUNT(DISTINCT albuns.album) AS albuns
 FROM
     musicas
         INNER JOIN
-    albuns
+    albuns ON musicas.album_id = albuns.id
         INNER JOIN
-    artistas
-WHERE
-    musicas.album_id = albuns.id
-        AND albuns.artista_id = artistas.id;
+    artistas ON albuns.artista_id = artistas.id;
--- desafio3.sql
+++ desafio3.sql
@@ -1,17 +1,12 @@
-USE SpotifyClone;
-
 SELECT 
     usuarios.usuario,
-    COUNT(historico_reproduzidas.musica_id) AS qtde_musicas_ouvidas,
-    CAST(SUM(musicas.duracao) / 60 AS DECIMAL (5 , 2 )) AS total_minutes
+    COUNT(historico.musica_id) AS qtde_musicas_ouvidas,
+    CAST(SUM(musicas.duracao) / 60 AS DECIMAL (4 , 2 )) AS total_minutes
 FROM
     usuarios
         INNER JOIN
-    historico_reproduzidas
+    historico ON usuarios.id = historico.usuario_id
         INNER JOIN
-    musicas
-WHERE
-    usuarios.id = historico_reproduzidas.usuario_id
-        AND historico_reproduzidas.musica_id = musicas.id
+    musicas ON historico.musica_id = musicas.id
 GROUP BY usuarios.usuario
 ORDER BY usuarios.usuario;
