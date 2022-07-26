b1361133db369c0312635eb47abe6dd0dc3c7f9b
Sandoerton <sandoerton@gmail.com>
2022-07-25 21:11:47 -0300

    fix: review desafio 7 e 8

--- desafio7.sql
+++ desafio7.sql
@@ -1,17 +1,12 @@
-USE SpotifyClone;
-
 SELECT 
     artistas.artista,
     albuns.album,
-    COUNT(artista_favorito.artista_id) AS seguidores
+    COUNT(favoritos.artista_id) AS seguidores
 FROM
-    artista_favorito
+    favoritos
         INNER JOIN
-    albuns
+    albuns ON favoritos.artista_id = albuns.artista_id
         INNER JOIN
-    artistas
-WHERE
-    artista_favorito.artista_id = albuns.artista_id
-        AND albuns.artista_id = artistas.id
-GROUP BY artistas.artista, albuns.album
-ORDER BY seguidores DESC, artistas.artista , albuns.album;
+    artistas ON albuns.artista_id = artistas.id
+GROUP BY artistas.artista , albuns.album
+ORDER BY seguidores DESC , artistas.artista , albuns.album;
--- desafio8.sql
+++ desafio8.sql
@@ -1,11 +1,8 @@
-USE SpotifyClone;
-
 SELECT 
     artistas.artista, albuns.album
 FROM
     artistas
         INNER JOIN
-    albuns
+    albuns ON artistas.id = albuns.artista_id
 WHERE
-    artistas.id = albuns.artista_id
-        AND artistas.artista = 'Walter Phoenix';
+    artistas.artista = 'Walter Phoenix';
